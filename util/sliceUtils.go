b898d92b5f91e5c3d111e2ce6a00c34c46747366
liujia <redQueen@163.com>
2023-12-22 15:03:22 +0800

    字符串截取

--- util/sliceUtils.go
+++ util/sliceUtils.go
@@ -1,5 +1,13 @@
 package util
 
+type ErrorMsg struct {
+	msg string
+}
+
+func (e ErrorMsg) Errors() string {
+	return e.msg
+}
+
 // IsEmptySliStr 判断切片string是否为空
 func IsEmptySliStr(arr []string) bool {
 	if arr == nil {
@@ -18,4 +26,14 @@ func StringAdd(arr string, arrAdd string) string {
 	byte1 = append(byte1, byte2...)
 	arr = string(byte1)
 	return arr
+}
+
+// StringSub 字符串截取(index)
+func StringSub(arr string, begin int, end int) (string, error) {
+	if end > len(arr) || begin < 0 || end < 0 {
+		err := &ErrorMsg{"索引越界"}
+		panic(err.msg)
+	}
+	arr2 := arr[begin:end]
+	return arr2, nil
 }
